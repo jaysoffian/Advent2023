463499e7c4b409d5bd5d8e6a81be9f5ecd93c677
Jay Soffian <jaysoffian@gmail.com>
2023-12-02 17:09:26 -0500

    day2: refactor

--- day02/day02.py
+++ day02/day02.py
@@ -11,19 +11,6 @@ SAMPLE = (HERE / "sample.txt").read_text()
 INPUT = (HERE / "input.txt").read_text()
 
 
-def split_line(line: str) -> tuple[int, str]:
-    """
-    Split game line.
-
-    >>> split_line("Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green")
-    (1, '3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green')
-    """
-    m = re.search(r"^Game (\d+): (.*)", line)
-    assert m is not None
-    game_number, grabs = m.groups()
-    return int(game_number), grabs
-
-
 def parse_grab(grab: str) -> tuple[int, int, int]:
     """
     Parse single grab. Return (red, green, blue).
@@ -40,42 +27,51 @@ def parse_grab(grab: str) -> tuple[int, int, int]:
     return counts["red"], counts["green"], counts["blue"]
 
 
-def parse_game1(line: str) -> int:
+def parse_line(line: str) -> tuple[int, tuple[tuple[int, int, int], ...]]:
+    """
+    Parse game line.
+
+    >>> parse_line("Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green")
+    (1, ((4, 0, 3), (1, 2, 6), (0, 2, 0)))
+    """
+    m = re.search(r"^Game (\d+): (.*)", line)
+    assert m is not None
+    game_number, grabs = m.groups()
+
+    return int(game_number), tuple(parse_grab(g) for g in grabs.split("; "))
+
+
+def parse_line_1(line: str) -> int:
     """
     Parse game line. Return game number if valid, else 0.
 
-    >>> parse_game1("Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green")
+    >>> parse_line_1("Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green")
     1
-    >>> parse_game1("Game 1: 3 blue, 4 red; 1 red, 2 green, 15 blue; 2 green")
+    >>> parse_line_1("Game 1: 3 blue, 4 red; 1 red, 2 green, 15 blue; 2 green")
     0
     """
-    game_number, grabs = split_line(line)
+    game_number, grabs = parse_line(line)
 
-    def is_valid_grab(grab: str) -> bool:
-        r, g, b = parse_grab(grab)
-        return r <= 12 and g <= 13 and b <= 14
-
-    for grab in grabs.split("; "):
-        if not is_valid_grab(grab):
+    for r, g, b in grabs:
+        if r > 12 or g > 13 or b > 14:
             return 0
 
     return game_number
 
 
-def parse_game2(line: str) -> int:
+def parse_line_2(line: str) -> int:
     """
     Parse game line. Return its power.
 
-    >>> parse_game2("Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green")
+    >>> parse_line_2("Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green")
     48
-    >>> parse_game2("Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue")
+    >>> parse_line_2("Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue")
     12
     """
-    game_number, grabs = split_line(line)
+    game_number, grabs = parse_line(line)
 
     max_r, max_g, max_b = 0, 0, 0
-    for grab in grabs.split("; "):
-        r, g, b = parse_grab(grab)
+    for r, g, b in grabs:
         max_r = max(max_r, r)
         max_g = max(max_g, g)
         max_b = max(max_b, b)
@@ -83,15 +79,15 @@ def parse_game2(line: str) -> int:
     return max_r * max_g * max_b
 
 
-def part1(lines) -> int:
-    return sum(parse_game1(line) for line in lines)
+def part1(lines: list[str]) -> int:
+    return sum(parse_line_1(line) for line in lines)
 
 
-def part2(lines) -> int:
-    return sum(parse_game2(line) for line in lines)
+def part2(lines: list[str]) -> int:
+    return sum(parse_line_2(line) for line in lines)
 
 
-def test():
+def test() -> None:
     lines = INPUT.splitlines()
     assert part1(SAMPLE.splitlines()) == 8
     assert part1(lines) == 2164
@@ -99,7 +95,7 @@ def test():
     assert part2(lines) == 69929
 
 
-def main():
+def main() -> None:
     lines = INPUT.splitlines()
     print(part1(lines))
     print(part2(lines))
